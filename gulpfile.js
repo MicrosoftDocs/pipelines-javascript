b15510a1f4a468b36c1e67ca68fb0e520dc3db2b
Vinod Joshi <vinojos@microsoft.com>
2018-08-21 13:18:12 +0530

    Include istanbul code coeverage with gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,9 @@
 var gulp = require('gulp');
 var mocha = require('gulp-mocha');
-var istanbul = require('gulp-istanbul') //my change
+var istanbul = require('gulp-istanbul')
 var fs = require('fs');
 
 gulp.task('default', function () {
-   // gulp.src('tests/**/*.js')
-    // Covering files
-     // .pipe(istanbul())
-    // Force `require` to return covered files
-      //.pipe(istanbul.hookRequire());
-    
     gulp.src('tests/**/*.js')
         .pipe(mocha({
           reporter: 'mocha-junit-reporter',
@@ -19,8 +13,8 @@ gulp.task('default', function () {
         }))
         .pipe(istanbul())
         .pipe(istanbul.hookRequire())
-        .pipe(istanbul.writeReports({    // my change
+        .pipe(istanbul.writeReports({
           dir: './coverage',
-          reporters: ['cobertura', 'html', 'json'] 
+          reporters: ['cobertura', 'html'] 
         }));
 });
