d3937d5cb5ad2a99c006f42c16728d85240f467d
Decade <yong_323@hotmail.com>
2019-03-04 11:20:11 +0800

    change test

--- tests/test.js
+++ tests/test.js
@@ -3,7 +3,7 @@ var assert = require('assert');
 describe('String', function () {
     describe('#indexOf', function () {
         it('should return -1 when the substring is not present', function () {
-            var myString = 'test';
+            var myString = 'tests';
 
             assert.equal(-1, myString.indexOf('x'));
             assert.equal(-1, myString.indexOf('y'));
