75b5788d4dd51528eaf4898479877f20a76d3874
Glenn Mercier <gmercier1986@hotmail.com>
2019-04-02 14:39:27 -0400

    newFilechange

--- HelloWorld.ps1
+++ HelloWorld.ps1
@@ -4,3 +4,4 @@ Param(
 )
 Write-Host "Hello world" from $greeter
 Write-Host Trigger: $trigger
+Write-Host "Now that you've got CI/CD, you can automatically deploy your app every time your team checks in code."
