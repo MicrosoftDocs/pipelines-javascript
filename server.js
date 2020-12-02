0a8914ba1f63437a75c75c27f7798ff77bf92a05
tjohn123456 <70169036+tjohn123456@users.noreply.github.com>
2020-12-01 23:30:33 -0800

    Update server.js

--- server.js
+++ server.js
@@ -14,4 +14,4 @@ app.get('/', (req, res) => {
 
 var port = process.env.PORT||PORT;
 app.listen(port);
-console.log(`Running on http://${HOST}:${PORT}`);
+console.log(`Running on the http://${HOST}:${PORT}`);
