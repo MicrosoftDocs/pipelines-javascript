ecac0353282a4802d4fa1a021279f56046a80730
silviuandrica <2338510+silviuandrica@users.noreply.github.com>
2021-11-18 12:20:39 +0100

    Adding more user info to the response header

--- server.js
+++ server.js
@@ -8,9 +8,10 @@ const HOST = '0.0.0.0';
 
 // App
 const app = express();
-app.get('/:user', (req, res) => {
+app.get('/:user/:location', (req, res) => {
   res.append('User', req.params.user);
-  res.send(`Hello, ${req.params.user}`);
+  res.append('Location', req.params.location);
+  res.send(`Hello, ${req.params.user} from ${req.params.location}`);
 });
 
 var port = process.env.PORT||PORT;
