69a788486a46bc04715b96b57e61fd96cc948052
silviuandrica <2338510+silviuandrica@users.noreply.github.com>
2021-11-18 14:49:44 +0100

    hello, world

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/:user/:location', (req, res) => {
-  res.send(`Hello, ${req.params.user} from ${req.params.location}`);
+  res.send(`Hello, world`);
 });
 
 var port = process.env.PORT||PORT;
