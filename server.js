22e4f998fbb842958a8b574b350ed81fbcc53b04
Silviu Andrica <2338510+silviuandrica@users.noreply.github.com>
2021-11-26 15:02:29 +0100

    Update server.js

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/:user/:location', (req, res) => {
+  res.append('Location', req.params.location);
   res.send(`Hello, ${req.params.user} from ${req.params.location}`);
 });
 
